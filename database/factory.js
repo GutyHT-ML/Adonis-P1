9b112855afa235da4d085da974cf22af224a8c02
GutyHT-ML <19170050@uttcampus.edu.mx>
2021-01-11 22:53:31 -0600

    Factories y Seeders terminados

--- database/factory.js
+++ database/factory.js
@@ -12,10 +12,12 @@
 */
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
-// const Factory = use('Factory')
-
-// Factory.blueprint('App/Models/User', (faker) => {
-//   return {
-//     username: faker.username()
-//   }
-// })
+const Factory = use('Factory')
+const Hash = use('Hash')
+Factory.blueprint('App/Models/User', async (faker) => {
+  return {
+    username: faker.username(),
+    email: faker.email(),
+    password: await Hash.make(faker.password())
+  }
+})
